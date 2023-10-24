3dc6f199cabd56ada6c83d21b706e6d08af8a529
JDean8 <jdean8@live.co.uk>
2023-10-24 11:42:37 +0100

    refectored code based on feedback, votes is now a state and error state setting happens in component, also resolved a bug causing an infinite loop

--- src/utils/articles_api.js
+++ src/utils/articles_api.js
@@ -22,46 +22,6 @@ export const getArticleComments = (article_id) => {
   });
 };
 
-export const likeArticle = (article_id, setArticle) => {
-  setArticle((currentArticle) => {
-    currentArticle += 1;
-    return currentArticle;
-  });
-  return articlesAPI
-    .patch(`/${article_id}`, { inc_votes: 1 })
-    .then((response) => {
-      console.log(response.data.article.votes);
-      return response.data.article.votes;
-    })
-    .catch((err) => {
-      console.log(err);
-      let error = document.getElementById(`article-vote-error-${article_id}`);
-      error.innerText = " - Serverside error, please try again later";
-      setArticle((currentArticle) => {
-        currentArticle.votes -= 1;
-        return currentArticle;
-      });
-    });
-};
-
-export const dislikeArticle = (article_id, setArticle) => {
-  setArticle((currentArticle) => {
-    currentArticle -= 1;
-    return currentArticle;
-  });
-  return articlesAPI
-    .patch(`/${article_id}`, { inc_votes: -1 })
-    .then((response) => {
-      console.log(response.data.article.votes);
-      return response.data.article.votes;
-    })
-    .catch((err) => {
-      console.log(err);
-      let error = document.getElementById(`article-vote-error-${article_id}`);
-      error.innerText = " - Serverside error, please try again later";
-      setArticle((currentArticle) => {
-        currentArticle.votes += 1;
-        return currentArticle;
-      });
-    });
+export const voteArticle = (article_id, increment) => {
+  return articlesAPI.patch(`/${article_id}`, { inc_votes: increment });
 };
